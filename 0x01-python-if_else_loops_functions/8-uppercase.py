5262f6b052cec5b5ebfe90485e0d16055bbde161
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-06 02:58:24 +0300

    pycodestyle fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,19 +5,20 @@ def isupper(c):
     else:
         return (False)
 
+
 def uppercase(str):
     word = ''
 
     for letter in str:
-        if isupper(letter) == True:
+        if isupper(letter) is True:
             upperletter_ascii = ord(letter)
             word = word + chr(upperletter_ascii)
         elif letter == ' ':
-            word += ' ' 
+            word += ' '
         elif ord(letter) >= 97 and ord(letter) <= 122:
             upperletter_ascii = ord(letter) - 32
             word = word + chr(upperletter_ascii)
         else:
             word += letter
-        
+
     print("{}".format(word))
