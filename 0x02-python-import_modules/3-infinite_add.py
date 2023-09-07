d391a7ef409fc812dfcbc0f863818aa25f13a791
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-07 20:57:18 +0300

    fixed to not execute when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-args = sys.argv
-args_len = len(args)
-result = 0
+    args = sys.argv
+    args_len = len(args)
+    result = 0
 
-for i in range(1, args_len):
-    result += int(args[i])
+    for i in range(1, args_len):
+        result += int(args[i])
 
-print(result)
+    print(result)
