e4855a2165d7a0e05d5ec23cad755af4dd41d903
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-07 18:07:14 +0300

    print the returned value directly

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    result = add(1, 2)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
