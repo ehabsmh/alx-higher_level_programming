00e0a8de7ecfad87fae35be319ca64bb0dbeaf67
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-11 01:21:44 +0300

    If list is None handled

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    sorted_list = sorted(my_list)
-    sorted_list.sort(reverse=True)
 
-    for n in sorted_list:
+    if my_list is None:
+        print("", end='')
+        return
+
+    my_list.sort(reverse=True)
+
+    for n in my_list:
         print("{:d}".format(n))
