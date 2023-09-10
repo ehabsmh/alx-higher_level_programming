db5f1af7a41421f8577ed36899e60346d9a23c32
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-11 01:18:11 +0300

    Original list not affected

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
+    sorted_list = sorted(my_list)
+    sorted_list.sort(reverse=True)
 
-    for n in my_list:
+    for n in sorted_list:
         print("{:d}".format(n))
