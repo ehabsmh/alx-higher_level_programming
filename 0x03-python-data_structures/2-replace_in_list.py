790a4212ea487991aa207a23e6243f2a504ba760
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-11 01:05:56 +0300

    Initial commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """ Replaces an element at specific pos """
+    list_len = len(my_list)
+
+    if ((idx < 0) or (idx > list_len - 1)):
+        return (my_list)
+
+    my_list[idx] = element
+    return (my_list)
