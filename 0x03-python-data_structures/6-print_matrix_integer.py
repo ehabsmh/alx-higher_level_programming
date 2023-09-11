e784c04d9a26a3d06de55de02d6549c22272c3c0
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-11 18:20:00 +0300

    decimal format added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         row_len = len(row)
         for n in range(row_len):
-            print("{}".format(row[n]), end=' ' if n < row_len - 1 else '')
+            print("{:d}".format(row[n]), end=' ' if n < row_len - 1 else '')
 
         print()
