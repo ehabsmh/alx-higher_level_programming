029220dcc1b145ea0c982a15543362cda77d9e0f
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-12 00:34:26 +0300

    Program refactored

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,6 +6,8 @@
  * @head: the head of Linked list
  * Return: 1 if palindrome OTHERWISE 0
 */
+
+/*
 int is_palindrome(listint_t **head)
 {
 	listint_t *trav1 = *head, *trav2 = *head;
@@ -14,14 +16,12 @@ int is_palindrome(listint_t **head)
 	if (!*head)
 		return (1);
 
-	/*Calculate length of linked-list*/
 	while (trav1)
 	{
 		counter++;
 		trav1 = trav1->next;
 	}
 
-	/* Reusing trav1 */
 	trav1 = *head;
 
 	for (i = 0; i < counter; i++)
@@ -41,5 +41,46 @@ int is_palindrome(listint_t **head)
 	}
 
 	trav2 = NULL;
+	return (1);
+}
+*/
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *traverse = *head;
+	int len = 0, i, j;
+	int *arr;
+
+	if (!*head)
+		return (1);
+
+	while (traverse)
+	{
+		len++;
+		traverse = traverse->next;
+	}
+
+	arr = malloc(sizeof(int) * len);
+	if (!arr)
+		return (0);
+
+	traverse = *head;
+
+	for (i = 0; i < len; i++)
+	{
+		arr[i] = traverse->n;
+		traverse = traverse->next;
+	}
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (arr[i] != arr[j])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+
+	free(arr);
 	return (1);
 }
