7708ea949a22ba6d0b8e7daccb6ec7822de4e6e9
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-12 00:38:48 +0300

    Error handled in linked list traversing

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,50 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * is_palindrome - Checks if a linked list is palindrome or not
  * @head: the head of Linked list
  * Return: 1 if palindrome OTHERWISE 0
 */
-
-/*
-int is_palindrome(listint_t **head)
-{
-	listint_t *trav1 = *head, *trav2 = *head;
-	int counter = 0, i, j, num;
-
-	if (!*head)
-		return (1);
-
-	while (trav1)
-	{
-		counter++;
-		trav1 = trav1->next;
-	}
-
-	trav1 = *head;
-
-	for (i = 0; i < counter; i++)
-	{
-		if (trav1)
-			num = trav1->n;
-
-		for (j = counter - 1; j > i; j--)
-			if (trav2)
-				trav2 = trav2->next;
-
-		if (trav2 && num != trav2->n)
-			return (0);
-
-		trav1 = trav1->next;
-		trav2 = *head;
-	}
-
-	trav2 = NULL;
-	return (1);
-}
-*/
-
 int is_palindrome(listint_t **head)
 {
 	listint_t *traverse = *head;
@@ -68,8 +30,11 @@ int is_palindrome(listint_t **head)
 
 	for (i = 0; i < len; i++)
 	{
-		arr[i] = traverse->n;
-		traverse = traverse->next;
+		if (traverse)
+		{
+			arr[i] = traverse->n;
+			traverse = traverse->next;
+		}
 	}
 
 	for (i = 0, j = len - 1; i < j; i++, j--)
