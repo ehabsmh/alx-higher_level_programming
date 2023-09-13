3862c1e770885ea3395c280b437c3f163b36b7d7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-14 02:46:23 +0300

    pycodestyle added

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,11 +8,11 @@ def roman_to_int(roman_string):
     size = len(roman_string)
 
     rom_lttrs = {"I": 1, "V": 5, "X": 10,
-         "L": 50, "C": 100, "D": 500, "M": 1000}
-    
+                 "L": 50, "C": 100, "D": 500, "M": 1000}
+
     for i in range(size):
         if (i < (size - 1) and rom_lttrs[roman_string[i]] <
-            rom_lttrs[roman_string[i + 1]]):
+                rom_lttrs[roman_string[i + 1]]):
             output -= rom_lttrs[roman_string[i]]
         else:
             output += rom_lttrs[roman_string[i]]
