1d4b3652dac075af20647f19ffd4d0c81af03d26
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-14 01:49:05 +0300

    Initial commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+
+    if type(a_dictionary) is not dict:
+        return None
+
+    best_key = ""
+    maximum = 0
+    for k, v in a_dictionary.items():
+        if v > maximum:
+            best_key = k
+            maximum = v
+
+    return best_key
