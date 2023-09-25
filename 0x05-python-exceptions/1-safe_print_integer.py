e5bd569e57c2d697cdcd29df448cc4207470ab66
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-25 15:40:07 +0300

    Initial commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """ prints an integer with "{:d}".format(). """
+    try:
+
+        print("{:d}".format(value))
+
+    except ValueError:
+
+        return (False)
+    
+    else:
+
+        return (True)
