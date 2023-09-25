7d37259c1f85a6477c328cc88cf52e29e57aea65
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-26 01:04:08 +0300

    Initial commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    """
+    prints an integer.
+    Returns True if value has been correctly printed
+    Otherwise, returns False
+    and prints in stderr the error precede by Exception:
+    """
+    try:
+
+        print("{:d}".format(value))
+        return True
+
+    except (ValueError) as err:
+
+        print(f"Exception: {err}", file=stderr)
+        return False
