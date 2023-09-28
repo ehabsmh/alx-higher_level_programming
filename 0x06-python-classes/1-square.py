2b9d03d5770a7071c235e698cd1cb46cb63a834f
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-28 18:20:23 +0300

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Define a class Square """
+
+
+class Square:
+
+    """ Initialize a new square
+
+    Args:
+        size: The size of the new square.
+    """
+    def __init__(self, __size):
+
+        self.__size = __size
