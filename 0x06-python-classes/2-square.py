4bc02d23f0fab84d480d09674bdb376744b3be21
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-28 18:40:54 +0300

    Initial commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" Define a class Square """
+
+
+class Square:
+
+    """ Initialize a new square
+
+    Args:
+        size: The size of the new square.
+    """
+    def __init__(self, __size=0):
+
+        self.__size = __size
+
+        """ size must be an integer """
+        if not isinstance(__size, int):
+
+            raise TypeError("size must be an integer")
+
+        """ size cannot be less than 0 """
+        if __size < 0:
+
+            raise ValueError("size must be >= 0")
