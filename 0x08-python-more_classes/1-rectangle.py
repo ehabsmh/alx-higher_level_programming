9aaea992cd6435359e3e829a25f722faee106f07
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-03 01:32:30 +0300

    Initial commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,72 +1,54 @@
 #!/usr/bin/python3
 """
-    This module contains a class Rectangle
+Module 2-rectangle
+Contains class Rectangle
+with private attribute width and height
 """
 
 
 class Rectangle:
     """
-    This is a class with initialized fields, and
-    encapsulated data.
+    Defines class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
     """
 
     def __init__(self, width=0, height=0):
-        """
-        Intialization of optional fields
-
-        Args:
-            width (int): if not given, default is 0
-            height (int): if not given, default is 0
-        """
-        self.__width = width
-        self.__height = height
+        """ Initialize rectangles """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """
-        Sets and returns the width of rectangle
-
-        Args:
-            value (int): must be an integer and >= 0
-
-        Returns:
-            An integer value
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is < 0
-        """
+        """ Getter returns width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Setter sets width if int > 0 """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """
-        Sets and returns the height of rectangle
-
-        Args:
-            value (int): must be an integer and >= 0
-        Returns:
-            An integer value
-        Raises:
-            TypeError: if value is not an integer
-            ValuError: if value is < 0
-        """
+        """ Getter returns height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ Setter sets height if int > 0 """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
-        else:
-            self.__height = value
+        self.__height = value
