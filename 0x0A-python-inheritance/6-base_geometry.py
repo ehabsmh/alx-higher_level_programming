23cf7b15e4ed4b4343c95441f6bc57e9b8188ce4
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-10 01:05:09 +0300

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" This module contains a class `BaseGeometry` """
+
+
+class BaseGeometry:
+    """define class BaseGeometry"""
+
+    def area(self):
+        """raises an Exception"""
+
+        raise Exception("area() is not implemented")
