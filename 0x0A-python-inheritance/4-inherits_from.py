62f14c72bf1f6816a9ce1fb9e7736d032cd330ab
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-10 00:54:39 +0300

    .

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -25,4 +25,4 @@ def inherits_from(obj, a_class):
       False: if the object is not an instance of the class.
     """
 
-    return isinstance(obj, a_class) and obj.__class__ is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
