1da88f70b8afec5e34532ac3767a2fac546dbbb7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-10 03:51:42 +0300

    initial commit

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""""This module contains only one function"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Adds a new attribute to an object if it's possible
+    """
+    if hasattr(obj, '__dict__'):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
