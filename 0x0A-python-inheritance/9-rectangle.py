83734e3e1b484a3bc3a83b53cde85b75cbca7c39
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-10 03:20:03 +0300

    initial commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module contains a class Rectangle"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initializes object"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format((type(self).__name__,
+                                    self.__width, self.__height))
