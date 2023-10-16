22a9f532df24ad60d9814942bc5b253c0e724cde
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-16 22:29:30 +0300

    to_json_string method added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 This module manages id attribute in subclasses
 """
 
+import json
+
 
 class Base:
     """Defines objects of type Base"""
@@ -25,3 +27,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    # ___________________________________________________________________
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries: a list of dictionaries
+
+        Return: "[]" if list_dictionaries is None or empty OTHERWISE,
+                return the JSON string representation of list_dictionaries.
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
