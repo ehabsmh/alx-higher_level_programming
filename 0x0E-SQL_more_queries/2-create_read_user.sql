91918bd857fbb98902b5f855cc98903ae24c5e76
Ehab Elsayed <ehabsmh3@gmail.com>
2023-11-15 21:52:35 +0200

    .

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- creates the MySQL server user user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
