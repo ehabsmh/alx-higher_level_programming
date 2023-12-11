383be02dacce05ca24b4cd894ec35bef7d17fb33
Ehab Elsayed <ehabsmh3@gmail.com>
2023-12-11 22:37:18 +0200

    logic fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,6 @@ const { argv } = require('node:process');
 
 console.log(argv);
 
-if (argv.length === 2) console.log('No argument');
+if (argv.length < 3) console.log('No argument');
 else if (argv.length === 3) console.log('Argument found');
 else console.log('Arguments found');
