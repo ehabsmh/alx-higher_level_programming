7f19117a975e95467725dfe9d83402e1d38b03f7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-12-11 22:39:26 +0200

    Removed the log

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-console.log(argv);
-
 if (argv.length < 3) console.log('No argument');
 else if (argv.length === 3) console.log('Argument found');
 else console.log('Arguments found');
