f4d48369408aefb19c589aaab6075371ce89bb23
Ehab Elsayed <ehabsmh3@gmail.com>
2023-12-12 00:26:32 +0200

    semistandard fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const argv = process.argv;
 
-if (argv.length < 3 || argv.length == 3) console.log(0);
+if (argv.length < 3 || argv.length === 3) console.log(0);
 else {
   argv.sort((a, b) => a - b);
   console.log(argv[argv.length - 2]);
