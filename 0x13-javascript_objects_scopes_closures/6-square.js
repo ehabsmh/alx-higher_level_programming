d86f9a820ec97ba1aa5a9845c90afea0bbfe97d4
Ehab Elsayed <ehabsmh3@gmail.com>
2023-12-13 00:16:13 +0200

    5-square imported

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle.js');
+const SquareModel = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+class Square extends SquareModel {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint (c='X') {
-    for (let i = 0; i < this.width; i++) {
-      console.log(c.repeat(this.width));
-    }
+  charPrint (c = 'X') {
+    this.print(c);
   }
-};
+}
+
+module.exports = Square;
