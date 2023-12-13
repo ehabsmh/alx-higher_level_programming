a63378bcb423bcc8435f398357945810a3ffef26
Ehab Elsayed <ehabsmh3@gmail.com>
2023-12-13 03:42:22 +0200

    13. Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const fileSys = require('fs');
+
+let argv = process.argv;
+
+argv = argv.slice(2);
+const srcFile1 = argv[0];
+const srcFile2 = argv[1];
+const destFile = argv[2];
+
+const arr = [srcFile1, srcFile2];
+
+for (let i = 0; i < arr.length; i++) {
+  fileSys.readFile(arr[i], 'utf8', (err, data) => {
+    if (err) throw err;
+
+    fileSys.appendFile(destFile, data, (err) => {
+      if (err) throw err;
+      console.log('saved!');
+    });
+  });
+}
