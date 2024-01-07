041fcb47e9831fc6de8b2a8005afe9127898d904
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-07 18:41:07 +0200

    cannot be executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """displays the value of the X-Request-Id variable found in the header of the\
  response."""
+
 from sys import argv
 from urllib.request import urlopen
 
-with urlopen(argv[1]) as response:
-    req_id = response.getheader('X-Request-Id')
-    print(req_id)
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        req_id = response.getheader('X-Request-Id')
+        print(req_id)
