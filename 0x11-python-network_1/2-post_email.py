89920c2a386f79b23e069446d9f6fa20affaf2ee
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-07 20:19:45 +0200

    refactored

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """Sends a POST request to the passed URL with the email as a parameter,\
  and displays the body of the response"""
 
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 from urllib import parse
 from sys import argv
 
@@ -10,9 +10,8 @@ if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
 
-    data = parse.urlencode(value)
-    data = data.encode('ascii')
+    data = parse.urlencode(value).encode('UTF-8')
+
     with urlopen(url, data) as response:
-        body = response.read()
-        body.decode('UTF-8')
+        body = response.read().decode('UTF-8')
         print(body)
