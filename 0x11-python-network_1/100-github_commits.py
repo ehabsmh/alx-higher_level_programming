3c21a583434bc6bbf5a6d55968b711bb3877622f
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-08 00:00:36 +0200

    refactored

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials (username and password) and uses the\
- GitHub API to display your id."""
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 
+import sys
 import requests
-from sys import argv
+
+
 if __name__ == "__main__":
-    payload = ""
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
-
-    response = requests.get(url).json()
-    ten_commits = response[:10]
-    
-    dates = []
-
-    for i in range(10):
-      commit_date = response[i].get('commit').get('author').get('date')
-      dates.append(commit_date)
-
-    dates.sort()
-
-    for date in dates:
-      for i in range(len(ten_commits)):
-        commit_date = response[i].get('commit').get('author').get('date')
-        commit_author = response[i].get('commit').get('author').get('name')
-        commit_sha = response[i].get('sha')
-        if date == commit_date:
-          print(f"{commit_sha}: {commit_author}")
-          del ten_commits[i]
-        
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
