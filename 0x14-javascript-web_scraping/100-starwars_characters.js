b52d7a338c240959f3bd732c678de5e4589cf3ee
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-30 22:46:42 +0200

    7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+let movieID = process.argv.slice(2, 3);
+
+if (movieID.length) {
+  movieID = movieID.toString();
+} else {
+  console.log('Usage: ./3-starwars_title.js movie_ID');
+  process.exit(1);
+}
+
+const endpoint = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request.get(endpoint, { json: true }, (err, res, body) => {
+  try {
+    const movie = body;
+    movie.characters.forEach(char => {
+      request.get(char, { json: true }, (err, res, body) => {
+        try {
+          if (err) throw err;
+          console.log(body.name);
+        } catch {
+          console.log(err);
+        }
+      });
+    });
+
+    if (err) throw err;
+  } catch {
+    console.log(err);
+  }
+});
