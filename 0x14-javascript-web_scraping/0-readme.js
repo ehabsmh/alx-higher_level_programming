2de800d9ee7ab210c55bb598d0372e2424763ea5
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-30 19:11:17 +0200

    semistandard fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 const process = require('process');
-const fs = require('fs')
+const fs = require('fs');
 
-let filePath = process.argv.slice(2,3)
+let filePath = process.argv.slice(2, 3);
 
 if (filePath.length) {
-  filePath = filePath.toString()
+  filePath = filePath.toString();
 } else {
-  console.log("Usage: ./0-readme.js filePath");
+  console.log('Usage: ./0-readme.js filePath');
 }
 
-fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   try {
     console.log(data);
     if (err) throw err;
   } catch {
     console.log(err);
   }
-})
+});
