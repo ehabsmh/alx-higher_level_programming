d08b798684548c922f2b1dc9d8e33bc1cba6b827
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-30 23:16:29 +0200

    8. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Display the status code of a GET request.
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+function requestChar (characters, num) {
+	if (num >= characters.length) {
+		return;
+	}
+	request.get(characters[num], function (_error, _response, body) {
+		console.log(JSON.parse(body).name);
+		requestChar(characters, num + 1);
+	});
+}
+
+request.get(url, function (_error, _response, body) {
+	const characters = JSON.parse(body).characters;
+	requestChar(characters, 0);
+});
