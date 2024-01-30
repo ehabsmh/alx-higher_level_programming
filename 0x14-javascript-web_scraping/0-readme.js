2e5419f250ec7008d36a90a2a722a8055e1441e9
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-30 19:08:08 +0200

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs')
+
+let filePath = process.argv.slice(2,3)
+
+if (filePath.length) {
+  filePath = filePath.toString()
+} else {
+  console.log("Usage: ./0-readme.js filePath");
+}
+
+fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+  try {
+    console.log(data);
+    if (err) throw err;
+  } catch {
+    console.log(err);
+  }
+})
