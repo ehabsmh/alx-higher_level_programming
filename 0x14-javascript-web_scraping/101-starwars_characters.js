d2b9ebff9127dd08f923f8fdd60eadd3341e92c2
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-30 23:17:41 +0200

    semistandard fixed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,16 +5,16 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 function requestChar (characters, num) {
-	if (num >= characters.length) {
-		return;
-	}
-	request.get(characters[num], function (_error, _response, body) {
-		console.log(JSON.parse(body).name);
-		requestChar(characters, num + 1);
-	});
+  if (num >= characters.length) {
+    return;
+  }
+  request.get(characters[num], function (_error, _response, body) {
+    console.log(JSON.parse(body).name);
+    requestChar(characters, num + 1);
+  });
 }
 
 request.get(url, function (_error, _response, body) {
-	const characters = JSON.parse(body).characters;
-	requestChar(characters, 0);
+  const characters = JSON.parse(body).characters;
+  requestChar(characters, 0);
 });
