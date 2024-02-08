2447a9428e1a1f8d1fabbaec196143ef1eb5b14d
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-08 20:50:17 +0200

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // When the DOM loads, select the <header> element
+  const header = document.querySelector('header');
+  //  update the text color to red
+  header.style.color = '#FF0000';
+});
